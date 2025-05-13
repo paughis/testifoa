3a9fbd0d7c3f9471b46a8915b555ff0b7cc3be1b
paughis <pghisonim@gmail.com>
2025-05-13 10:32:51 +0200

    ho aggiunto info

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
     st.title('✨:rainbow[prima app]✨')
-
+    st.subheader('Tutorial bandana')
     st.text('https://www.youtube.com/watch?v=c9BugUy_xB0')
 
 if __name__ == "__main__":
